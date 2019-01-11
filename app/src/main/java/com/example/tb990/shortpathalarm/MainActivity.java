7857f8fd886a2960f8663beab7a13d7e122b21d9
supreme5876 <supreme5876@naver.com>
2019-01-11 22:51:49 +0900

    marker test 1.0

--- app/src/main/java/com/example/tb990/shortpathalarm/MainActivity.java
+++ app/src/main/java/com/example/tb990/shortpathalarm/MainActivity.java
@@ -205,13 +205,12 @@ public class MainActivity extends AppCompatActivity implements TMapGpsManager.on
     public void showMarkerPoint(TMapPoint point) {// 마커 찍는거 빨간색 포인트.
         TMapMarkerItem markerItem1 = new TMapMarkerItem();
         TMapPoint tMapPoint1 = point; // SKT타워
-
-// 마커 아이콘
-      //  Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.locicon);
-      //  markerItem1.setIcon(bitmap); // 마커 아이콘 지정
         markerItem1.setPosition(0.5f, 1.0f); // 마커의 중심점을 중앙, 하단으로 설정
         markerItem1.setTMapPoint( tMapPoint1 ); // 마커의 좌표 지정
         markerItem1.setName("도착지"); // 마커의 타이틀 지정
+        markerItem1.setCanShowCallout(true); // 풍선뷰 사용 여부
+        markerItem1.setCalloutTitle("도착지...");
+        markerItem1.setCalloutSubTitle("long:"+point.getLongitude()+"\nlati:"+point.getLatitude());
         tmapview.addMarkerItem("markerItem1", markerItem1); // 지도에 마커 추가
         tmapview.setCenterPoint( point.getLongitude(), point.getLatitude() );
     }
